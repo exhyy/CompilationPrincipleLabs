8699d397aae132feabf8dfb92677a23e859c57fe
exhyy <starryhyy@gmail.com>
2022-11-22 10:37:53 +0800

    update

--- Lab04/include/PrecedenceTable.h
+++ Lab04/include/PrecedenceTable.h
@@ -1,12 +1,32 @@
 #ifndef PRECEDENCETABLE_H_
 #define PRECEDENCETABLE_H_
 
+#include "utils.h"
 #include <fstream>
+#include <string>
+#include <set>
+#include <utility>
+#include <map>
+
+#define PRECEDENCE_LT -1
+#define PRECEDENCE_EQ 0
+#define PRECEDENCE_GT 1
+
+typedef std::pair<std::string, std::string> PSS;
 
 class PrecedenceTable
 {
 private:
     std::ifstream _infile;
+    std::set<std::string, SymbolCmp> _terminal;
+    std::set<std::string, SymbolCmp> _nonterminal;
+    std::map<PSS, int> _data;
+    void _getNonterminal();
+    void _getTerminal();
+    void _getData();
+
+public:
+    PrecedenceTable(std::string filename);
 };
 
 #endif
--- Lab04/src/PrecedenceTable.cpp
+++ Lab04/src/PrecedenceTable.cpp
@@ -1,2 +1,65 @@
-#include "ParsingTable.h"
+#include "PrecedenceTable.h"
+
+PrecedenceTable::PrecedenceTable(std::string filename)
+{
+    _infile.open(filename, std::ios::in);
+    if (!_infile.is_open())
+    {
+        std::string msg = "无法打开" + filename;
+        throw msg;
+    }
+
+}
+
+void PrecedenceTable::_getNonterminal()
+{
+    _infile.clear();
+    _infile.seekg(0, std::ios::beg);
+    std::string line;
+    while (!_infile.eof())
+    {
+        getline(_infile, line);
+        int index = line.find("->");
+        if (index == -1)
+            continue;
+        std::string nonterminal = line.substr(0, index);
+        _nonterminal.insert(nonterminal);
+    }
+}
+
+void PrecedenceTable::_getTerminal()
+{
+    _infile.clear();
+    _infile.seekg(0, std::ios::beg);
+    std::string line;
+    while (!_infile.eof())
+    {
+        getline(_infile, line);
+        int index = line.find("->");
+        if (index == -1)
+            continue;
+        std::string right = line.substr(index + 2);
+
+        // 删除非终结符
+        for (auto n : _nonterminal)
+        {
+            index = right.find(n);
+            while (index != -1)
+            {
+                right.erase(index, n.length());
+                index = right.find(n);
+            }
+        }
+        std::vector<std::string> subStrings = splitString(right, "|");
+        for (auto str : subStrings)
+        {
+            for (int i = 0; i < int(str.length()); i++)
+            {
+                std::string terminal = "";
+                terminal += str[i];
+                _terminal.insert(terminal);
+            }
+        }
+    }
+}
 
