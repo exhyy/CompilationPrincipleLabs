250cc2e3d000585f52aaa639b5729d51a898309e
exhyy <starryhyy@gmail.com>
2022-12-18 19:31:47 +0800

    Lab05: update main.cpp

--- Lab05/src/main.cpp
+++ Lab05/src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include "Grammar.h"
 #include "Item.h"
 #include "ItemSets.h"
@@ -9,52 +8,41 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc > 5)
+    {
+        std::cerr << "参数过多" << std::endl;
+        return 1;
+    }
+    if (argc < 4)
+    {
+        std::cerr << "至少需要3个参数：文法文件、输入文件和输出文件" << std::endl;
+        return 1;
+    }
+    bool debug = false;
+    if (argc == 5 && std::string(argv[4]) == "--debug")
+        debug = true;
     Grammar grammar(argv[1], "\\epsilon", "#");
-    std::cout << "Grammar构建完成" << std::endl;
     ItemSets itemSets(grammar, true);
-    std::cout << "ItemSets构建完成" << std::endl;
     if (itemSets.isAugmented())
         std::cout << "文法已增广" << std::endl;
-
     FirstSet firstSet(grammar);
-    // std::cout << "FirstSet: " << std::endl;
-    // firstSet.print();
-
     FollowSet followSet(grammar, firstSet);
-    // std::cout << std::endl
-            //   << "FollowSet: " << std::endl;
-    // followSet.print();
-    itemSets.printParsingTable();
 
     SLR1Parser parser(grammar, itemSets);
-    int t = parser.parse(argv[2], argv[3], true);
+
+    if (debug)
+    {
+        std::cout << "SLR(1)分析表如下：" << std::endl;
+        itemSets.printParsingTable();
+        std::cout << std::endl
+                  << "分析过程如下：" << std::endl;
+    }
+    int t = parser.parse(argv[2], argv[3], debug);
     if (t == 0)
         std::cout << "语法分析成功" << std::endl;
     else
         std::cout << "语法分析失败" << std::endl
                   << "错误位置：" << t << std::endl
                   << "错误信息：" << parser.errorMessage() << std::endl;
-
-    // auto itemSetsData = itemSets.data();
-    // for (int i = 0; i < int(itemSetsData.size()); i++)
-    // {
-    //     std::cout << "Item Set " << i << ": " << std::endl;
-    //     for (auto item : itemSetsData[i])
-    //     {
-    //         std::cout << item.rule().left << "->";
-    //         auto right = item.rule().right;
-    //         for (int j = 0; j < int(right.size()); j++)
-    //         {
-    //             if (j == item.dot())
-    //                 std::cout << "·";
-    //             std::cout << right[j];
-    //         }
-    //         if (item.dot() == int(right.size()))
-    //             std::cout << "·";
-    //         std::cout << std::endl;
-    //     }
-    //     std::cout << std::endl
-    //               << std::endl;
-    // }
     return 0;
 }
